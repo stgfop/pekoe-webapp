4f769000e350f00e8dcd3edbf4f49d9bc9f14ae1
Duncan Berenguier <duncan.berenguier@gmail.com>
2015-03-13 11:17:01 +0100

    working architecture with controller and websocketServise

--- web/js/controller/pekoe-controller.js
+++ web/js/controller/pekoe-controller.js
@@ -1,3 +1,56 @@
-angular.module('PekoeApp').controller('PekoeController', ['PekoeWebsocket', '$scope', function(ws, $scope) {
+'use strict';
+
+angular.module('PekoeApp').controller('PekoeController', ['$scope', '$rootScope', 'PekoeWebsocketService', function($scope, $rootScope, PekoeWebsocketService) {
+
+        var eventHandlers = new Map();
+        $scope.lobby = {
+            games: [],
+            gamesCount: "loading..."
+        }
+        $rootScope.eventChannel = {
+            listeners: [],
+            listen: function(o) {
+                this.listeners.push(o);
+            },
+            recieve: function(e) {
+                for (var i = 0; i < this.listeners.length; i++) {
+                    var l = this.listeners[i];
+                    l(e);
+                }
+
+            }
+        };
+
+        init();
+
+        function init() {
+
+            $rootScope.eventChannel.listen(eventDispatcher);
+
+            $scope.ws = PekoeWebsocketService.ws();
+
+            eventHandlers.set("me.meeoo.server.event.PekoeListGameEvent", eventHandler_listGames);
+            eventHandlers.set("me.meeoo.server.event.PekoeNewGameEvent", null);
+            eventHandlers.set("me.meeoo.server.event.PekoeNewPlayerEvent", null);
+            eventHandlers.set("me.meeoo.server.event.PekoeStartGameEvent", null);
+
+        }
+
+        function eventDispatcher(event) {
+            var eventHandler = eventHandlers.get(event.name);
+            if (eventHandler == null) {
+                console.log("Could not dispatch unkown event", event.name, event);
+            } else {
+                console.log("dispatching event to", event.name, event);
+                eventHandler(event);
+            }
+            $scope.$apply();
+        }
+
+        function eventHandler_listGames(event) {
+            console.log("Got the list");
+            $scope.lobby.games = event.games;
+            $scope.lobby.gamesCount = event.gamesCount;
+        }
 
     }]);
--- web/js/controller/pekoe-websocket.js
+++ web/js/controller/pekoe-websocket.js
@@ -1,34 +1,42 @@
 'use strict';
 
-angular.module('PekoeApp').controller('PekoeController', ['PekoeWebsocket', '$scope', function(ws, $scope) {
+angular.module('PekoeApp', ['ngWebsocket']).service('EventCommunicationChannel', function() {
+    var ecc = {
+        event: null,
+        newEvent: setEvent
+    };
+    return ecc;
 
-}]);
+    function setEvent(e) {
+        ecc.event = e;
+    }
+}).service('PekoeWebsocketService', ['$rootScope', '$websocket', function($rootScope, $websocket) {
 
-angular.module('PekoeWebsocket', ['ngWebsocket']).run(
-        function($websocket) {
+        return {
+            ws: getWS,
+        };
+
+        function getWS() {
             var ws = $websocket.$new({
                 url: 'ws://localhost:8080/pekoe/ws',
                 reconnect: true // it will reconnect after 2 seconds
             }); // instance of ngWebsocket, handled by $websocket service
+            ws.status = "disconnected";
 
             ws.$on('$open', function() {
-                console.log('Oh my gosh, websocket is really open! Fukken awesome!');
-
-                console.log('connecting')
+                console.log("WEBSOCKET: $open");
+                ws.status = "connected";
                 ws.$emit('me.meeoo.server.event.PekoeListGameEvent', ["", 0]); // send a message to the websocket server
-
-                console.log('pong...')
-                // ws.$emit('pong', ['one', 'two', 'three']);
             });
 
             ws.$on('me.meeoo.server.event.PekoeListGameEvent', function(data) {
-                console.log(data);
+                console.log("WEBSOCKET<-event ", data);
                 var event = {};
                 event.name = data.shift();
                 event.transactionId = data.shift();
-                event.gameCount = data.shift();
+                event.gamesCount = data.shift();
                 event.games = [];
-                for (var i = 0; i < event.gameCount; i++) {
+                for (var i = 0; i < event.gamesCount; i++) {
                     var game = {};
                     game.creation = data.shift();
                     game.visualHash = data.shift();
@@ -37,18 +45,12 @@ angular.module('PekoeWebsocket', ['ngWebsocket']).run(
                     game.isStarted = data.shift();
                     event.games.add(game);
                 }
-                console.log(event);
-            });
-
-            ws.$on('pong', function(data) {
-                console.log('The websocket server has sent the following data:');
-                console.log(data);
-
-                ws.$close();
+                $rootScope.eventChannel.recieve(event);
             });
 
             ws.$on('$close', function() {
-                console.log('Noooooooooou, I want to have more fun with ngWebsocket, damn it!');
+                ws.status = "disconnected";
+                console.log('WEBSOCKET: $close recieved');
             });
 
             ws.$on('$error', function(error) {
@@ -56,7 +58,7 @@ angular.module('PekoeWebsocket', ['ngWebsocket']).run(
                 console.log(error);
             });
 
-            ws.$on('$message', function() {
-                console.log(message);
-            });
-        });
+
+            return ws;
+        }
+    }]);
